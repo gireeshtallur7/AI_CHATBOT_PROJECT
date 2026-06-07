5d9205c5cea8dafabcedcb9a9022110777d45e6f
gireeshtallur7 <girishtallur07@gmail.com>
2026-06-07 09:10:13 +0530

    Removed unrelated folders

--- chat_models/embeddings.py
+++
@@ -1,7 +0,0 @@
-from langchain_openai import OpenAIEmbeddings
-from dotenv import load_dotenv
-load_dotenv()
-
-embeddings = OpenAIEmbeddings(model="text-embedding-3-small",dimensions=64)
-result = embeddings.embed_query("What is Gen AI ?")
-print(result)   
